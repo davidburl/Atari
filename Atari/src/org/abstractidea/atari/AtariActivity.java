d692fada02ee8f8fdbbccee257a00e1f530d2623
Matthew Vaughn <abstract@mabv.org>
2012-04-21 19:37:19 -0400

    just added whitespace for testing git source control

--- Atari/src/org/abstractidea/atari/AtariActivity.java
+++ Atari/src/org/abstractidea/atari/AtariActivity.java
@@ -12,10 +12,45 @@ import android.os.Bundle;
  * 
  * HEY!
  * 
- * @author David, Matthew
+ * @author David Burl, Matthew A.B. Vaughn
+ * @since  2012/03/25 22:23:22
  */
 public class AtariActivity extends SimpleBaseGameActivity {
-    /** Called when the activity is first created. */
+	/**
+	 * The width of the camera view.
+	 */
+	private static final int CAMERA_WIDTH = 480;
+	/**
+	 * The height of the camera width.
+	 */
+	private static final int CAMERA_HEIGHT = 800;
+	/**
+	 * The number of rows and columns to draw
+	 */
+	private static final int NUM_ROWSCOLS = 19;
+	/**
+	 * The size of the edge
+	 */
+	private static final float BORDER_SIZE = 0.01f;
+	/**
+	 * The top border (leaving room for a HUD)
+	 */
+	private static final float TOP_BORDER = 0.25f * CAMERA_HEIGHT;
+	/**
+	 * First space on the board
+	 */
+	private static final float FIRST_SPACE = BORDER_SIZE * CAMERA_WIDTH;
+	/**
+	 * Amount of space between each row
+	 */
+	private static final float SPACING = (CAMERA_WIDTH * (1 - (2 * BORDER_SIZE)))
+			/ NUM_ROWSCOLS;
+	
+	// removed, due to redundancy in HudZoomListener
+	private static final float MAX_ZOOM = 4f;
+	private static final float MIN_ZOOM = 1f;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,18 +58,18 @@ public class AtariActivity extends SimpleBaseGameActivity {
         
         
     }
-
+    
 	public EngineOptions onCreateEngineOptions() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 	@Override
 	protected void onCreateResources() {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
 	protected Scene onCreateScene() {
 		// TODO Auto-generated method stub
